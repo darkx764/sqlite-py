1ef24f8d45771610e5933a2149cd7e6dd655d360
darkx764 <adasyurx024@gmail.com>
2026-01-01 14:12:32 -0700

    check if folder exist to avoid error

--- classes/databases.py
+++ classes/databases.py
@@ -4,6 +4,7 @@ from classes.cli_prompter import menu
 class databases:
     """ Databases Manager ."""
     def __init__(self):
+        # try to check if database folder exists or not
         pass
 
     def list(self, path):
--- classes/file.py
+++ classes/file.py
@@ -4,6 +4,12 @@ class file_management:
     """ File Management Class. """
     @staticmethod
     def save(d = 'start_application'):
-        # save state into file 
-        with open('storage/user.log', 'w', encoding='utf-8') as state:
-            state.write(d) # write user state into file
+        # try to create storage directory
+        try:
+            os.mkdir('storage')
+        # if file exists
+        except FileExistsError: 
+            # save state into file 
+            with open('storage/user.log', 'w', encoding='utf-8') as state:
+                state.write(d) # write user state into file
+        
